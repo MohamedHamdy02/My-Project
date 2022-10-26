2e8142d01051b46509b5131a2db7d435e4fb4d37
MohamedHamdy02 <mohamedghazy900@gmail.com>
2022-10-26 16:02:31 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// import { Typed } from "https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js";
+
+var typed = new Typed(".header__title", {
+    strings: ["Web Designer", "Photographer", "Web Developer" , "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true,
+});
